dc40e4b3559c01573e94f120a325371d306e7b3f
wellfix <dbrozyna15@gmail.com>
2022-08-24 12:31:14 +0200

    feat: Added enum messages to exceptions. Renamed enums so it's more fitting. Deleted validation from inside transformer.

--- src/Validator/PaymentObjectValidator.php
+++ src/Validator/PaymentObjectValidator.php
@@ -4,6 +4,7 @@ namespace NetsCore\Validator;
 
 use NetsCore\Dto\NextAccept\Request\PaymentObject;
 use NetsCore\Enums\CurrencyCodeEnum;
+use NetsCore\Enums\ExceptionEnum;
 use NetsCore\Enums\LanguageEnum;
 use NetsCore\Enums\PageTypeEnum;
 use NetsCore\Enums\PaymentProcessingTypeEnum;
@@ -15,32 +16,32 @@ class PaymentObjectValidator
     public static function isPaymentObjectValid(PaymentObject $paymentObject): bool
     {
         if (empty($paymentObject->redirectUrls)) {
-            throw new PaymentObjectException("redirectUrls must be RedirectUrls object");
+            throw new PaymentObjectException(ExceptionEnum::REDIRECTURLS_CANNOT_BE_EMPTY);
         }
         if ( ! PaymentTypeEnum::isValid($paymentObject->type)) {
-            throw new PaymentObjectException("Invalid payment type");
+            throw new PaymentObjectException(ExceptionEnum::PAYMENT_TYPE_IS_INVALID);
         }
 
         if (empty($paymentObject->amount)) {
-            throw new PaymentObjectException("Amount variable cannot be empty");
+            throw new PaymentObjectException(ExceptionEnum::PAYMENT_AMOUNT_CANNOT_BE_EMPTY);
         }
 
         if ( ! PaymentProcessingTypeEnum::isValid($paymentObject->processing)) {
-            throw new PaymentObjectException("Invalid processing type");
+            throw new PaymentObjectException(ExceptionEnum::PAYMENT_PROCESSING_TYPE_ERROR);
         }
 
         if ( ! CurrencyCodeEnum::isValid($paymentObject->currencyCode)) {
-            throw new PaymentObjectException("Invalid currency code");
+            throw new PaymentObjectException(ExceptionEnum::PAYMENT_CURRENCY_CODE_ERROR);
         }
 
         if ( ! empty($paymentObject->payPageConfiguration)) {
             if (LanguageEnum::isValid($paymentObject->payPageConfiguration->language)) {
-                throw new PaymentObjectException("PayPageConfiguration->language is invalid");
+                throw new PaymentObjectException(ExceptionEnum::PAYPAGE_CONFIGURATION_LANGUAGE_ERROR);
             } elseif (PageTypeEnum::isValid($paymentObject->payPageConfiguration->pageType)) {
-                throw new PaymentObjectException("PayPageConfiguration->pageType is invalid");
+                throw new PaymentObjectException(ExceptionEnum::PAYPAGE_CONFIGURATION_PAGETYPE_ERROR);
             }
         } else {
-            throw new PaymentObjectException("PayPageConfiguration cannot be empty");
+            throw new PaymentObjectException(ExceptionEnum::PAYPAGE_CONFIGURATION_ERROR);
         }
 
         return true;
