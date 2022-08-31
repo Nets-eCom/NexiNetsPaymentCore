122854eb207e154ab77d8bfd45ffaa365b32af41
wellfix <dbrozyna15@gmail.com>
2022-08-31 08:46:57 +0200

    hotfix: changed enum names, so they are working

--- src/Enums/ExceptionEnum.php
+++ src/Enums/ExceptionEnum.php
@@ -6,13 +6,13 @@ use MyCLabs\Enum\Enum;
 
 class ExceptionEnum extends Enum
 {
-    public const REDIRECTURL_CANNOT_BE_EMPTY = 'RedirectUrl has to be fill';
+    public const REDIRECTURLS_CANNOT_BE_EMPTY = 'redirectUrls must be RedirectUrls object';
     public const PAYMENT_TYPE_IS_INVALID = 'Payment type that was used to process a payment is not valid';
     public const PAYMENT_AMOUNT_CANNOT_BE_EMPTY = 'Amount has to be indicated';
-    public const WRONG_PAYMENT_PROCESING_TYPE = 'Invalid Processing Type';
-    public const CURRENCY_ERROR = 'Wrong currency code';
-    public const PAYPAGE_CONFIGURATION_LANGUAGE_ERROR = 'Language is invalid';
-    public const PAYPAGE_CONFIGURATION_PAGETYPE_ERROR = 'PageType is invalid';
+    public const PAYMENT_PROCESSING_TYPE_ERROR = 'Invalid Processing Type';
+    public const PAYMENT_CURRENCY_CODE_ERROR = 'Wrong currency code';
+    public const PAYPAGE_CONFIGURATION_LANGUAGE_ERROR = 'Pay Page Language is invalid';
+    public const PAYPAGE_CONFIGURATION_PAGETYPE_ERROR = 'Pay Page PageType is invalid';
     public const PAYPAGE_CONFIGURATION_ERROR = 'Pay Page cannot be empty';
     public const CAPTURE_PAYMENT_CRITICAL_ERROR = 'Capture Payment Validation Fail';
     public const CAPTURE_PAYMENT_BAD_REQUEST = 'Capture Payment Bad Request';
