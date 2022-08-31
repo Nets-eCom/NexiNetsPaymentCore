f03aa6894ac8dec14a6257f5c15768f5060d2d93
wellfix <dbrozyna15@gmail.com>
2022-08-31 09:56:56 +0200

    feat: Added Cart Value validator

---
+++ src/Validator/CartValueValidator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace NetsCore\Validator;
+
+use NetsCore\Dto\Netaxept\BasketItemDto;
+use NetsCore\Enums\ExceptionEnum;
+use NetsCore\Exceptions\CartValueException;
+
+class CartValueValidator
+{
+    /**
+     * @param BasketItemDto[] $basket
+     * @param int $requestAmount
+     *
+     * @return bool
+     */
+    public static function cartValueValidator(array $basket, int $requestAmount): bool
+    {
+        $cartValue = 0;
+        foreach ($basket as $item){
+            $cartValue += $item->quantity * $item->unitPrice;
+        }
+        if ($cartValue == $requestAmount){
+            return true;
+        }
+        else
+        {
+            throw new CartValueException(ExceptionEnum::CART_VALUE_EXCEPTION);
+        }
+    }
+
+
+}
