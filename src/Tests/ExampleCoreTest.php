6f85254245db74ad58fca3427694969e5ae556af
wellfix <dbrozyna15@gmail.com>
2022-09-13 16:12:19 +0200

    feat: PHPUnit init inside core

---
+++ src/Tests/ExampleCoreTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NetsCore\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleCoreTest extends TestCase
+{
+    public function test_if_phpunit_works() : void {
+        $this->assertTrue(true);
+    }
+}
