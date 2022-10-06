9e43a8c3205b14c206d5bf731532ac652724f789
bates <bilgebilginates@gmail.com>
2022-10-06 13:14:45 +0200

    feature/15603  added mockery and run the test

---
+++ src/Test/TestHelper.php
@@ -0,0 +1,15 @@
+<?php
+namespace Nets\Payment\Test;
+
+use \Mockery\Adapter\Phpunit\MockeryTestCase;
+
+class TestHelper extends MockeryTestCase
+{
+    /**@test**/
+    public function testExample(): void
+    {
+        $this->assertFalse(false);
+
+    }
+
+}
