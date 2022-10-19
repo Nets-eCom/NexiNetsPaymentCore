777ffc6d7110fe89103a09f8065e49738255529c
wellfix <dbrozyna15@gmail.com>
2022-10-20 00:36:51 +0200

    feat: Sandbox init

--- src/Enums/ClientTypeEnum.php
+++ src/Enums/ClientTypeEnum.php
@@ -8,4 +8,5 @@ final class ClientTypeEnum extends Enum
 {
     public const EASY_NETS = 'NetsEasy';
     public const NETAXEPT = 'Netaxept';
+    public const NETAXEPT_SANDBOX = 'NetaxeptSandbox';
 }
