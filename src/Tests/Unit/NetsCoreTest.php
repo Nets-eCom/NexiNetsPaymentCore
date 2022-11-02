f731fd184176b447765799ccba7eae752ab5d3f3
bates <bilgebilginates@gmail.com>
2022-11-02 03:19:17 +0100

    feature/16552 updated Test

--- src/Tests/Unit/NetsCoreTest.php
+++ src/Tests/Unit/NetsCoreTest.php
@@ -5,7 +5,9 @@ namespace NetsCore\Tests;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use NetsCore\Dto\Netaxept\Request\PaymentRequest;
 use NetsCore\Dto\Netaxept\Response\AuthorizePaymentResponseDto;
+use NetsCore\Dto\Netaxept\Response\CreatePaymentResponseDto;
 use NetsCore\Dto\Netaxept\Response\PaymentDetailResponseDto;
+use NetsCore\Interfaces\PaymentObjectInterface;
 use NetsCore\NetsCore;
 
 class NetsCoreTest extends MockeryTestCase
@@ -35,6 +37,22 @@ class NetsCoreTest extends MockeryTestCase
         $this->assertInstanceOf(
             PaymentDetailResponseDto::class,
             $netsCoreMock->getPaymentDetails($paymentId)
+        );
+    }
+
+        public function testCreatePayment(): void
+    {
+        $netsCoreMock = \Mockery::mock(NetsCore::class)->makePartial();
+        $netsCoreMock->shouldAllowMockingProtectedMethods();
+        $netsCoreMock->shouldReceive('getClient->createPayment')->with(PaymentObjectInterface::class)->andReturn(
+            new CreatePaymentResponseDto()
+        );
+
+        $mockInterface=$this->createMock(PaymentObjectInterface::class);
+
+        $this->assertInstanceOf(
+            CreatePaymentResponseDto::class ,
+            $netsCoreMock->createPayment($mockInterface)
         );
     }
 
