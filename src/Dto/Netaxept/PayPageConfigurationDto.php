595b2c232f3af6b5de4caeb5224c15993d2448c7
stacho <staszekk1212@gmail.com>
2022-11-10 15:36:35 +0100

    Changes for preparing Dto for PayPageConfiguration

--- src/Dto/Netaxept/PayPageConfigurationDto.php
+++ src/Dto/Netaxept/PayPageConfigurationDto.php
@@ -7,8 +7,62 @@ class PayPageConfigurationDto
     /**
      * @var PaymentMethodActionInfoDto[]
      */
-    public array $paymentMethodActionInfo;
+    public ?array $paymentMethodActionInfo;
     public string $language;
     public string $pageType;
     public string $templateName;
+
+    /**
+     * @param string $language
+     * @return PayPageConfigurationDto
+     */
+    public function setLanguage(string $language): PayPageConfigurationDto
+    {
+        $this->language = $language;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLanguage(): string
+    {
+        return $this->language;
+    }
+
+    /**
+     * @param string $pageType
+     * @return PayPageConfigurationDto
+     */
+    public function setPageType(string $pageType): PayPageConfigurationDto
+    {
+        $this->pageType = $pageType;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPageType(): string
+    {
+        return $this->pageType;
+    }
+
+    /**
+     * @param string $templateName
+     * @return PayPageConfigurationDto
+     */
+    public function setTemplateName(string $templateName): PayPageConfigurationDto
+    {
+        $this->templateName = $templateName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTemplateName(): string
+    {
+        return $this->templateName;
+    }
 }
--- src/Interfaces/PaymentObjectInterface.php
+++ src/Interfaces/PaymentObjectInterface.php
@@ -4,5 +4,4 @@ namespace NetsCore\Interfaces;
 
 interface PaymentObjectInterface
 {
-    public function setLanguage(string $language);
 }
